5753af35ceeeb963d5738c62006821c0358dbafd
Predrag <predragzivkovic@elfak.rs>
2023-06-19 20:30:38 +0200

    initial

--- projekat/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ projekat/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -17,18 +17,26 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button buttonSviKorisnici;
+    Button buttonSviKorisnici,prikaziNaMapi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         auth= FirebaseAuth.getInstance();
         buttonSviKorisnici=findViewById(R.id.buttonVidiSveKorisnike);
+        prikaziNaMapi=findViewById(R.id.buttonMap);
         buttonSviKorisnici.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(LoginActivity.this,AllUsersActivity.class);
                 startActivity(intent);
+            }
+        });
+        prikaziNaMapi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(LoginActivity.this,MapActivity.class);
+                startActivity(i);
             }
         });
     }
